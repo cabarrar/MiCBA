a5a298165c1be0304cffe67f49a49a77cb545457
cabarrar <114768935+cabarrar@users.noreply.github.com>
2026-05-10 09:09:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,17 @@
-self.addEventListener('fetch', function(event) {
-    // Necesario para que la PWA sea detectada correctamente
+const CACHE_NAME = 'micba-v1';
+
+// Se ejecuta al instalar
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['./', './index.html', './manifest.json']);
+    })
+  );
+});
+
+// Necesario para que sea instalable
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
